4cd1802ab4a31c68dd43bb63e283cfbaefa102f3
nelauria <40785509+nelauria@users.noreply.github.com>
2020-09-15 15:54:16 -0700

    Update film_data.py

--- picture_preference/film_data.py
+++ picture_preference/film_data.py
@@ -12,9 +12,9 @@ def check_popularity(query):
         films = web_scrape.top(page=page).set_index(films.index+72)
     film_info = films.loc[films.href == href]
     rank = film_info.index[0]+1
-    print(f'{film_info.Title.iloc[0]} is ranked #{rank} in popularity.')
+    # print(f'{film_info.Title.iloc[0]} is ranked #{rank} in popularity.')
     total_films = 542018
     obscurity = (film_info.index[0]/total_films)*100
-    print(f'{film_info.Title.iloc[0]} is more obscure than {obscurity:.5f}% of films on Letterboxd.')
+    # print(f'{film_info.Title.iloc[0]} is more obscure than {obscurity:.5f}% of films on Letterboxd.')
     return [film_info.Title.iloc[0], rank, obscurity]
 
