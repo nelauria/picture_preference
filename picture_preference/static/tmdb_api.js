1c3ce591be9430b93f49264d20ad0451fc657d22
nelauria <40785509+nelauria@users.noreply.github.com>
2020-09-22 23:56:14 -0700

    heroku tests

--- picture_preference/static/tmdb_api.js
+++ picture_preference/static/tmdb_api.js
@@ -1,12 +1,12 @@
 $(document).on('input touchend', '#userInput', function(){
-//    var input = $(this).val();
-//    let div, ul, titles = [], images = [];
-//    $.ajax({
-//        url: $SCRIPT_ROOT + "_search",
-//        type: "get",
-//        data: {searchQuery: input},
-//        dataType: "text",
-//        success: function(data) {
+    var input = $(this).val();
+    let div, ul, titles = [], images = [];
+    $.ajax({
+        url: $SCRIPT_ROOT + "_search",
+        type: "get",
+        data: {searchQuery: input},
+        dataType: "text",
+        success: function(data) {
 //            dataJSON = JSON.parse(data)
 //            $.each(dataJSON, function(index, item) {
 //                title = item.title
@@ -23,9 +23,9 @@ $(document).on('input touchend', '#userInput', function(){
 //            if (data !== '[]') {
 //                $(".search-results").css("display", "block")
 //            }
-////            $("#test").html("changed")
-//        }
-//    })
-    $("#test").html("changed")
+            $("#test").html(data)
+        }
+    })
+//    $("#test").html("changed")
 });
 
