e188df406e11a5f1be5bb6681478ce7ea23ae395
nelauria <40785509+nelauria@users.noreply.github.com>
2020-09-23 00:11:16 -0700

    heroku tests

--- picture_preference/static/tmdb_api.js
+++ picture_preference/static/tmdb_api.js
@@ -14,8 +14,8 @@ $(document).on('input touchend', '#userInput', function(){
                 if (item.poster_path) {
                     posterLink = "https://image.tmdb.org/t/p/w92/" + item.poster_path;
                 } else {
-//                    posterLink = "/static/photo-placeholder-icon-17.jpg"
-                    posterLink = ""
+                    posterLink = "/static/photo-placeholder-icon-17.jpg"
+//                    posterLink = ""
                 }
                 $(".film-button").eq(index).html(buttonText)
                 $(".film-button").eq(index).attr("value", title + " (" + item.release_date.slice(0,4) + ")")
