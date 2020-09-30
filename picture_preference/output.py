b10ff83deca597bbd7ced41dc8c2a4c15cac0d5e
nelauria <40785509+nelauria@users.noreply.github.com>
2020-09-30 15:49:26 -0700

    update obscurity rating

--- picture_preference/output.py
+++ picture_preference/output.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, g, render_template, session
 )
 from .film_db import FilmModel
+import math
 
 bp = Blueprint('output', __name__)
 
@@ -15,10 +16,11 @@ def results():
     ).first() #change this to all when addressing duplicate films
     if g.film:
         g.rank = f"#{g.film.rank}"
-        g.obscurity = ((g.film.rank-1)/544834)*100
-        g.obscurity = "%.3g%%" % g.obscurity
+        g.popularity = math.exp(-(g.film.rank-1)/2000)
+        g.obscurity = (1-g.popularity)*10
+        g.obscurity = "%.2g" % g.obscurity
     else:
         g.rank = "Pretty damn low"
-        g.obscurity = "Inefficiently Obscure"
+        g.obscurity = "10"
 
     return render_template('results.html')
