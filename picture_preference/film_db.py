e0191d6866a31c91255da9669a2861b8c49d0fef
nelauria <40785509+nelauria@users.noreply.github.com>
2020-10-02 23:01:25 -0700

    add tmdb_id

--- picture_preference/film_db.py
+++ picture_preference/film_db.py
@@ -1,6 +1,7 @@
 from picture_preference import web_scrape
 from flask_sqlalchemy import SQLAlchemy, event
 from flask import current_app
+import time
 
 db = SQLAlchemy()
 
@@ -12,19 +13,21 @@ class FilmModel(db.Model):
     title = db.Column(db.String())
     rank = db.Column(db.Integer, unique=True)
     href = db.Column(db.String(), unique=True)
+    tmdb_id = db.Column(db.Integer)
 
-    def __init__(self, title, rank, href):
+    def __init__(self, title, rank, href, tmdb_id):
         self.title = title
         self.rank = rank
         self.href = href
+        self.tmdb_id = tmdb_id
 
     def __repr__(self):
         return f"<Film {self.title}>"
 
 
-@event.listens_for(FilmModel.__table__, 'after_create')
-@current_app.cli.command("db-reinit")
+@current_app.cli.command("db-fill")
 def build_top(*args, **kwargs):
+    time_start = time.time()
     FilmModel.query.delete()
     chapters = 5
     chapter_length = 25
@@ -32,9 +35,11 @@ def build_top(*args, **kwargs):
         print(f"Filling from chapter {chapter+1} ({chapter_length} pages)...")
         start = 1+chapter*chapter_length
         end = (chapter+1)*chapter_length
-        titles, ranks, hrefs = web_scrape.top_pages(start_page=start, end_page=end)
-        if not (len(titles) == len(ranks) == len(hrefs)):
-            raise Exception("Error in web_scrape.py: titles, ranks, hrefs are not of the same length.")
-        db.session.add_all([FilmModel(titles[i], ranks[i], hrefs[i]) for i in range(len(titles))])
+        titles, ranks, hrefs, tmdb_ids = web_scrape.top_pages(start_page=start, end_page=end)
+        if not (len(titles) == len(ranks) == len(hrefs) == len(tmdb_ids)):
+            raise Exception("Error in web_scrape.py: titles, ranks, hrefs, tmdb_ids are not of the same length.")
+        db.session.add_all([FilmModel(titles[i], ranks[i], hrefs[i], tmdb_ids[i]) for i in range(len(titles))])
         db.session.commit()
+    time_end = time.time()
     print(f"Top {72*(chapters*chapter_length)} films initialized.")
+    print(f"Time: {time_end-time_start}")
--- picture_preference/web_scrape.py
+++ picture_preference/web_scrape.py
@@ -1,11 +1,54 @@
-import requests
 from bs4 import BeautifulSoup
+from flask import current_app
+import requests
+
+
+def tmdb_id(title_year):
+    api_key = current_app.config["TMDB_KEY"]
+    search_url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US"
+    # print(title_year)
+    title = title_year[:-7]
+    title = title.replace(" ", "%20").replace("#", "%23")
+    try:
+        year = int(title_year[-5:-1])
+    except ValueError:
+        year = None
+    if year:
+        search_url = search_url + f"&query={title}&page=1&include_adult=false&year={year}"
+    else:
+        title = title_year
+        title = title.replace(" ", "%20").replace("#", "%23")
+        search_url = search_url+f"&query={title}&page=1&include_adult=false"
+    response = requests.get(search_url)
+    results = response.json()["results"]
+    if not results:
+        print(f"Year mismatch for {title_year}, searching in {year+1}...")
+        search_url = search_url + f"&query={title}&page=1&include_adult=false&year={year+1}"
+        response = requests.get(search_url)
+        results = response.json()["results"]
+        if not results:
+            print(f"Another year mismatch, searching in {year-1}...")
+            search_url = search_url + f"&query={title}&page=1&include_adult=false&year={year-1}"
+            response = requests.get(search_url)
+            results = response.json()["results"]
+            if not results:
+                print(f"Another year mismatch, searching in TV...")
+                search_url = search_url.replace("/movie?", "/tv?")
+                response = requests.get(search_url)
+                results = response.json()["results"]
+                if not results:
+                    print(f"No results for {title_year}, film_id = 0.")
+                    film_id = 0
+                    return film_id
+    film_id = results[0]["id"]
+    return film_id
 
 
 def top_pages(time_period='', start_page=1, end_page=10):
     titles = []
     ranks = []
     hrefs = []
+    tmdb_ids = []
     for page_num in range(start_page, end_page+1):
         url = 'https://letterboxd.com/films/ajax/popular/'+time_period+'/size/small/'
         if page_num != 1:
@@ -13,8 +56,9 @@ def top_pages(time_period='', start_page=1, end_page=10):
         page = requests.get(url)
         soup = BeautifulSoup(page.content,'html.parser')
         films_divs = soup.find_all(title=True)
-        for i in range(len(films_divs)):
-            titles.append(films_divs[i]['title'])
-            ranks.append(i+1+(page_num-1)*72)
-            hrefs.append(films_divs[i]['href'])
-    return titles, ranks, hrefs
+        iter_range = range(len(films_divs))
+        titles = [films_divs[i]['title'] for i in iter_range]
+        hrefs = [films_divs[i]['href'] for i in iter_range]
+        tmdb_ids = [tmdb_id(films_divs[i]['title']) for i in iter_range]
+        ranks = [i+1+(page_num-1)*72 for i in iter_range]
+    return titles, ranks, hrefs, tmdb_ids
