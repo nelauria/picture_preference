ee97712e8146f4dd2ff88070fc6ff36067c4d99f
nelauria <40785509+nelauria@users.noreply.github.com>
2020-10-06 22:13:55 -0700

    add mainstream and obscure

--- picture_preference/output.py
+++ picture_preference/output.py
@@ -16,8 +16,20 @@ def results():
     ).first() #change this to all when addressing duplicate films
     if g.film:
         g.rank = f"#{g.film.rank}"
-        g.popularity = math.exp(-(g.film.rank-1)/2000)
-        g.obscurity = (1-g.popularity)*10
+        g.popularity = math.exp(-(g.film.rank - 1) / 2000)
+        g.obscurity = (1 - g.popularity) * 10
+        if g.obscurity >= 0.75:
+            min_rank = 1 - 2000 * math.log(1 - (g.obscurity - 0.5) / 10)
+            min_rank = math.floor(min_rank)
+            g.mainstream = FilmModel.query.filter(
+                FilmModel.rank.between(min_rank, g.film.rank)
+            ).all()
+        if g.obscurity <= 9.0:
+            max_rank = 1 - 2000 * math.log(1 - (g.obscurity + 0.5) / 10)
+            max_rank = math.floor(max_rank)
+            g.obscure = FilmModel.query.filter(
+                FilmModel.rank.between(g.film.rank, max_rank)
+            ).all()
         g.obscurity = "%.2g" % g.obscurity
     else:
         g.rank = None
