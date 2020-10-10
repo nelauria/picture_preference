64bb7f9114bd5358da70b582d1a209c1ab9c1424
nelauria <40785509+nelauria@users.noreply.github.com>
2020-10-09 21:54:22 -0700

    Update web_scrape.py

--- picture_preference/web_scrape.py
+++ picture_preference/web_scrape.py
@@ -6,7 +6,7 @@ import requests
 def tmdb_id(title_year):
     api_key = current_app.config["TMDB_KEY"]
     search_url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US"
-    print(title_year)
+    # print(title_year)
     title = title_year[:-7]
     title = title.replace(" ", "%20").replace("#", "%23")
     try:
@@ -21,6 +21,7 @@ def tmdb_id(title_year):
         search_url = search_url+f"&query={title}&page=1&include_adult=false"
     response = requests.get(search_url)
     results = response.json()["results"]
+    medium = "movie"
     if not results:
         print(f"Year mismatch for {title_year}, searching in {year+1}...")
         search_url = search_url + f"&query={title}&page=1&include_adult=false&year={year+1}"
@@ -32,34 +33,42 @@ def tmdb_id(title_year):
             response = requests.get(search_url)
             results = response.json()["results"]
             if not results:
+                medium = "tv"
                 print(f"Another year mismatch, searching in TV...")
                 search_url = search_url.replace("/movie?", "/tv?")
                 response = requests.get(search_url)
                 results = response.json()["results"]
                 if not results:
+                    medium = None
                     print(f"No results for {title_year}, film_id = 0.")
                     film_id = 0
-                    return film_id
+                    return film_id, medium
     film_id = results[0]["id"]
-    return film_id
+    return film_id, medium
 
 
-def meta_soup(film_id):
-    api_key = current_app.config["TMDB_KEY"]
-    details_url = f"https://api.themoviedb.org/3/movie/{film_id}?api_key={api_key}&language=en-US&append_to_response=keywords,credits"
-    response = requests.get(details_url)
-    details = response.json()
-    genres = [i["name"].replace(" ", "").lower() for i in details["genres"]]
-    keywords = [i["name"].replace(" ", "").lower() for i in details["keywords"]["keywords"]]
-    if len(details["credits"]["cast"]) >= 3:
-        cast = [details["credits"]["cast"][i]["name"].replace(" ", "").lower() for i in range(0,3)]
-    else:
-        cast = [
-            details["credits"]["cast"][i]["name"].replace(" ", "").lower() for i in range(len(details["credits"]["cast"]))
-        ]
-    directors = [i["name"].replace(" ", "").lower() for i in details["credits"]["crew"] if i["job"] == "Director"]
-    metadata = genres + keywords + cast + directors
-    soup = " ".join(metadata)
+def meta_soup(film_id, medium):
+    soup = None
+    if medium:
+        api_key = current_app.config["TMDB_KEY"]
+        details_url = f"https://api.themoviedb.org/3/{medium}/{film_id}?api_key={api_key}&language=en-US&append_to_response=keywords,credits"
+        response = requests.get(details_url)
+        details = response.json()
+        genres = [i["name"].replace(" ", "").lower() for i in details["genres"]]
+        if medium == "movie":
+            keywords = [i["name"].replace(" ", "").lower() for i in details["keywords"]["keywords"]]
+        elif medium == "tv":
+            keywords = [i["name"].replace(" ", "").lower() for i in details["keywords"]["results"]]
+        if len(details["credits"]["cast"]) >= 3:
+            cast = [details["credits"]["cast"][i]["name"].replace(" ", "").lower() for i in range(0,3)]
+        else:
+            cast = [
+                details["credits"]["cast"][i]["name"].replace(" ", "").lower() for i in
+                range(len(details["credits"]["cast"]))
+            ]
+        directors = [i["name"].replace(" ", "").lower() for i in details["credits"]["crew"] if i["job"] == "Director"]
+        metadata = genres + keywords + cast + directors
+        soup = " ".join(metadata)
     return soup
 
 
@@ -82,8 +91,8 @@ def top_pages(time_period='', start_page=1, end_page=10):
             title = film_div['title']
             titles.append(title)
             hrefs.append(film_div['href'])
-            tmdb_id_i = tmdb_id(title)
+            tmdb_id_i, medium_i = tmdb_id(title)
             tmdb_ids.append(tmdb_id_i)
-            film_meta.append(meta_soup(tmdb_id_i))
+            film_meta.append(meta_soup(tmdb_id_i, medium_i))
             ranks.append(i+1+(page_num-1)*72)
     return titles, ranks, hrefs, tmdb_ids, film_meta
